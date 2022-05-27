b3836db8a35150904b239f5004b82fc4de5bf7ec
DIEGO FERREIRA ALVES VAZ <diego.favaz@senacsp.edu.br>
2022-05-27 11:54:12 -0300

    first commit

---
+++ exercicios.js
@@ -0,0 +1,14 @@
+/*
+var num1 = parseInt(prompt('Digite um número para somar: '))
+var num2 = parseInt(prompt('Digite outro número para somar: '))
+
+alert(num1 + num2) 
+*/
+
+var idade = prompt('Digite sua idade')
+
+if (idade >= 18){
+    alert('Você é maior de idade')
+} else {
+    alert('Você é menor de idade')
+}
